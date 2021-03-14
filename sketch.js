d4b95f29b6831b4489d59875d42262805e8f0184
NEO20-08 <rujuta20vaidya@gmail.com>
2021-03-14 11:53:42 +0530

    created Chain.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine,world,ground;
 var box1,box2,box3,box4,box5;
 var ground,platform;
 var pig1,pig2;
-var log1,log2,log3,log4;
+var log1,log2,log3,log4,constrainedLog;
 var bird;
 var backGroundImg;
+
 
 function preload(){
   backGroundImg = loadImage("sprites/bg.png");
@@ -23,7 +25,7 @@ ground = new Ground(600,390,1200,20);
 platform = new Ground(150,305,300,170);
 box1 = new Box(700,320,70,70);
 box2 = new Box(920,320,70,70);
-pig1 = new Pig(810,350)
+pig1 = new Pig(810,350);
 log1 = new Log(810,260,300,PI/2);
 
 box3 = new Box(700,240,70,70);
@@ -36,6 +38,10 @@ log3 = new Log(760,120,150,PI/7);
 log4 = new Log(870,120,150,-PI/7);
 
 bird = new Bird(100,100)
+constrainedLog = new Log(230,180,80,PI/2);
+chain = new Chain(bird.body,constrainedLog.body);
+
+
 }
 
 function draw() {
@@ -56,4 +62,6 @@ box3.display();
   log3.display();
 log4.display();
 bird.display();
+constrainedLog.display();
+chain.display();
 }
