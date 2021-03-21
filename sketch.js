74c874f67e18952f1b297c270d59cf3fac1aa889
NEO20-08 <rujuta20vaidya@gmail.com>
2021-03-21 10:48:31 +0530

    updated SlingShot.js

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ log3 = new Log(760,120,150,PI/7);
 log4 = new Log(870,120,150,-PI/7);
 
 bird = new Bird(100,100);
-slingShot = new SlingShot(bird.body,{x:200,y:100});
+slingShot = new SlingShot(bird.body,{x:200,y:50});
 
 
 }
